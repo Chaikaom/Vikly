5494bafb79bf7e3bf1f9b224e3f7d0991b3a84e0
Oleksandr Chaika <chaykaa@ukr.net>
2018-12-05 16:09:05 +0200

    Commit3

---
+++ Vikly/src/test/java/homeTest/HomeTest.java
@@ -0,0 +1,26 @@
+package homeTest;
+
+import org.junit.Test;
+import pages.HomePage;
+import parentTest.ParentTest;
+
+import java.io.IOException;
+
+public class HomeTest extends ParentTest {
+    @Test
+    public void validAuth() throws  IOException {
+      homePage.openPage();
+      screenPage.TakeScreen();
+        homePage.click1();
+        screenPage.TakeScreen();
+        homePage.click2();
+        screenPage.TakeScreen();
+        homePage.click3();
+        screenPage.TakeScreen();
+        homePage.click4();
+        screenPage.TakeScreen();
+        homePage.click5();
+        screenPage.TakeScreen();
+    }
+
+}
